6f6605c058bc0a969b1c4caddc6996f8450005f3
AishwaryaMallampati <aishwarya.mallampati@gmail.com>
2020-11-21 12:59:09 -0500

    Added login activity

--- app/src/main/java/com/codepath/instagram/MainActivity.java
+++ app/src/main/java/com/codepath/instagram/MainActivity.java
@@ -3,8 +3,14 @@ package com.codepath.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
